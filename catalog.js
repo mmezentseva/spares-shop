5e884be56dacb367467cb549c550a8c522ab9f47
Marina Mezentseva <marinamezentseva07@gmail.com>
2020-05-14 13:27:22 +0300

    first page full PP

--- catalog.js
+++ catalog.js
@@ -1,17 +1,17 @@
 'use strict';
 
-// var select = document.querySelector('.filter-fieldset__roll');
-var selectTitle = document.querySelectorAll('.filter-fieldset__roll-title');
+var select = document.querySelector('.filter-fieldset__roll');
+var selectTitle = document.querySelector('.filter-fieldset__roll-title');
 var selectLabel = document.querySelectorAll('.filter-fieldset__roll-label');
 
-// Toggle menu
-/* selectTitle.addEventListener('click', function () {
+ // Toggle menu
+selectTitle.addEventListener('click', function () {
   if ('active' === select.getAttribute('data-state')) {
     select.setAttribute('data-state', '');
   } else {
     select.setAttribute('data-state', 'active');
   }
-}); */
+}); 
 
 // Close when click to option
 for (var i = 0; i < selectLabel.length; i++) {
@@ -19,16 +19,5 @@ for (var i = 0; i < selectLabel.length; i++) {
       var select = document.querySelector('.filter-fieldset__roll');
         selectTitle.textContent = evt.target.textContent;
         select.setAttribute('data-state', '');
-  });
-}
-
-for (var i = 0; i < selectTitle.length; i++) {
-  selectTitle[i].addEventListener('click', function () {
-    var select = document.querySelector('.filter-fieldset__roll');
-    if ('active' === select.getAttribute('data-state')) {
-      select.setAttribute('data-state', '');
-    } else {
-      select.setAttribute('data-state', 'active');
-    }
   });
 }
